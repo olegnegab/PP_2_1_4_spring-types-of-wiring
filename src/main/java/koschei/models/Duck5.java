398282d6da9283d73229dad860eee85a191aa7ae
Oleg <oleg.negab@gmail.com>
2023-06-08 10:02:26 +0300

    1

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -10,6 +10,7 @@ public class Duck5 {
 
     @Override
     public String toString() {
+
         return ", в утке яйцо " + egg6.toString();
     }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,12 +1,18 @@
 package koschei.models;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Component
+//@Component
 public class Needle7 {
+    private Deth8 deth8;
+
+    public Needle7( Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
+
         return ", смерть Кощея на игле :( " + "";
     }
 }
